b251e3e366b8e53e1db7ac2076d0b5e4f424fae1
Regan Perkins <reganlperkins@gmail.com>
2014-11-24 20:29:12 +0000

    movie validations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -33,5 +33,20 @@ class MoviesController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  protected
+
+  def movie_params
+    params.require(:movie).permit(
+      :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
+    )
+  end
 
 end
