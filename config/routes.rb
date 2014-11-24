a975e78e408fa86b2f00f4cde5ec3c9d2460effa
Regan Perkins <reganlperkins@gmail.com>
2014-11-24 21:07:14 +0000

    add login funcitionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+  get 'users/create'
+  resources :sessions, only: [:new, :create]
+
   # get 'movies/index'
 
   # get 'movies/show'
