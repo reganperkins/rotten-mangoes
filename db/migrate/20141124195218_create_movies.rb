0c8a459289bf0b724377c7b33b1c3374e5ee2bbe
Regan Perkins <reganlperkins@gmail.com>
2014-11-24 19:57:52 +0000

    movie model and migrate

---
+++ db/migrate/20141124195218_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.integer :runtime_in_minutes
+      t.string :descrip
+
+      t.timestamps
+    end
+  end
+end
