b7a6d602eb85e3f801ab6bfd1239c2f0d2a62d38
Regan Perkins <reganlperkins@gmail.com>
2014-11-27 18:09:53 +0000

    working on pagination

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,12 @@ class Admin::UsersController < ApplicationController
   before_action :confirm_admin
 
   def index
-    render :index
+  #@users = User.find(params[:id]).page(params[:id]).per(1)
+  # @users = User.all.page.per(10)
+  # @users = User.all.page(params[:page]).per(10)
+  # @users = User.where(role: 0).page(params[:page]).per(10)
+    @users = User.scoped.page(params[:page]).per(10)
+
   end
 
 
