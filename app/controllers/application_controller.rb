0ec0d7a5e0064e2b47a4c2bdd5952a11cc07462c
Regan Perkins <reganlperkins@gmail.com>
2014-12-05 02:46:57 +0000

    gemfile change for heroku deply

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  
-  @current_page = 'default'
- 
-
   protected
 
   helper_method :current_user
